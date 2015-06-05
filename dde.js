02655d7f67fbce318bf94482de455c0b1b11df9a
steven37788 <harrybeacher@gmail.com>
2015-06-05 11:42:38 +0800

    reflact dde.js and index.js， let dde.js only support util, index focus on business

--- dde.js
+++ dde.js
@@ -1,19 +1,12 @@
 var request = require('request');
 var Iconv = require('iconv-lite');
 var cheerio = require('cheerio');
-var mysql = require('./mysql.js');
 var logger = require('./applog').logger;
 
-//var log = function(str){
-//    var time=geddy.date.strftime(new Date(), '%Y.%m.%d %H:%M:%S')
-//    logger.info(time+': '+str);
-//}
 
 var headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
 }
-
-var data = '';
 
 function MyUtil() {
     this.sleep = function (time, callback) {
@@ -24,7 +17,7 @@ function MyUtil() {
         callback();
     },
 
-    this.load = function (url, cb) {
+    this.loadHomePage = function (url, dealData1) {
 
             request({
                     encoding: null,
@@ -50,68 +43,12 @@ function MyUtil() {
                     logger.info(exception);
                 }
 
-
-                if (typeof cb === 'function') {
-                    cb(null, null);
+                if (typeof dealData1 === 'function') {
+                    dealData1(myArray, ddx_update);
                 };
             };
-
-
-            if(typeof myArray=="undefined"){
-                logger.info('eval 未起效。');
-                logger.info(body);
-
-                return false;
-            } else {
-
-                //logger.info(myArray);
-                logger.info("*****************\n");
-
-                if(typeof ddx_update=="undefined"){
-                    alert('ddx_update 没有数值，请检查。');
-                    return false;
-                }
-
-                sortData(myArray);
-
-                var data = getData ();
-                //logger.info(data);
-                mysql.insertDataBase(data, ddx_update);
-
-                logger.info("MyUtil.load end\n");
-            }
         })
     }
-}
-
-function getData() {
-    return data;
-}
-
-function sortByNumber(n,s,p, myArray)
-{
-    if(typeof myArray=="undefined"){alert('eval 未起效。 请检查js文件。');return false;}
-
-    myArray.sort(function sortFun(x, y){
-            return s*(parseFloat(x[n])-parseFloat(y[n]));
-        }
-    );
-
-    logger.info('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
-
-    //logger.info(myArray);
-}
-
-function sortData(myArray) {
-
-    //logger.info(myArray);
-
-
-    sortByNumber(8, -1, 1, myArray);
-
-    data =  myArray;
-
-    //logger.info(myArray);
 }
 
 module.exports = new MyUtil();
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var async = require('async');
+//var async = require('async');
 
 var myUtil = require('./dde.js');
-//$ = require('jQuery');
-
 var logger = require('./applog').logger;
+var mysql = require('./mysql.js');
 
-
-//var qurl = "http://www.dingniugu.com/ddxdata20140919/ddedata.php?";
+var rooturl = "http://www.dingniugu.com";
 var qurl = "http://www.dingniugu.com/ddxdata/ddedata.php?";
+
 
 var index_url = qurl + 'pid=8&order=-1'+ "&rand=" + Math.round(Math.random() * 10000);
 var index_url1 = qurl + 'pid=50&order=-1'+ "&rand=" + Math.round(Math.random() * 10000);
@@ -15,41 +14,93 @@ var index_url1 = qurl + 'pid=50&order=-1'+ "&rand=" + Math.round(Math.random() *
 var arrStr = new Array();
 var arrFunc = new Array();
 
-
-
-
-var foo = function(index, results){
-    // results is now equal to index_url;
-    //logger.info(results);
-    logger.info(index);
-
-    //logger.info("This is an index page! -- log4js");
-
-
-
-    // 调用方法
-    myUtil.sleep(1000, function() {
-        // executes after one second, and blocks the thread
-        myUtil.load(index_url, function (err, data) {
-            if (err)
-                logger.info(err);
-        });
-    });
-};
-
-for(var i=0; i < 30; i++)
-{
-    // 调用方法
-    var index_url = qurl + 'pid=' + i.toString() + '&order=-1'+ "&rand=" + Math.round(Math.random() * 10000);
-
-    arrStr.push(index_url); //only for print
-
-
-    foo(i, index_url);
+var headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36'
 }
 
-logger.info(arrStr);
+var data = '';
+function getData() {
+    return data;
+}
 
+function sortByNumber(n,s,p, myArray)
+{
+    if(typeof myArray=="undefined"){
+        logger.info('eval 未起效。 请检查js文件。');
+        return false;
+    }
+
+    myArray.sort(function sortFun(x, y){
+            return s*(parseFloat(x[n])-parseFloat(y[n]));
+        }
+    );
+
+    logger.info('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+}
+
+function sortData(myArray) {
+
+    sortByNumber(8, -1, 1, myArray);
+
+    data =  myArray;
+}
+
+var dealData1 = function(myArray, ddx_update) {
+
+    if(typeof myArray=="undefined"){
+        logger.info('eval 未起效。');
+        logger.info(body);
+
+        return false;
+    } else {
+
+        //logger.info(myArray);
+        logger.info("*****************\n");
+
+        if(typeof ddx_update=="undefined"){
+            logger.info('ddx_update 没有数值，请检查。');
+            return false;
+        }
+
+        sortData(myArray);
+
+        var data = getData ();
+        //logger.info(data);
+        mysql.insertDataBase(data, ddx_update);
+
+        logger.info("MyUtil.loadHomePage end\n");
+    }
+}
+
+function getHomePageData () {
+    for (var i = 0; i < 6; i++) {
+        var index_url = qurl + 'pid=' + i.toString() + '&order=-1&rand=' + Math.round(Math.random() * 10000);
+
+        arrStr.push(index_url); //only for print
+
+        logger.info(i.toString());
+        //getData(i, index_url);
+
+        myUtil.sleep(1000, function() {
+            // executes after one second, and blocks the thread
+            myUtil.loadHomePage(index_url, dealData1);
+        });
+    }
+}
+
+getHomePageData ();
+//
+logger.info(arrStr);
+//
 logger.info('application end');
+
+function getStockCode () {
+    var stock_url = rooturl + '/newdata/stockcode.js';
+
+    myUtil.loadHomePage(stock_url, function (err, data) {
+        if (err)
+            logger.info(err);
+    });
+}
 
 
