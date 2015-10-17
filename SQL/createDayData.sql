3ec47473e0b0be1ecad93611a8fa3ae1f0e9202a
steven37788 <harrybeacher@gmail.com>
2015-10-17 13:52:59 +0800

    Update createDayData.sql

--- SQL/createDayData.sql
+++ SQL/createDayData.sql
@@ -8,7 +8,6 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `t_daydata`;
 
 CREATE TABLE `t_daydata`(
-id INT PRIMARY KEY AUTO_INCREMENT,
 openingprice       float(8,2) NOT NULL ,
 closingprice       float(8,2) NOT NULL ,
 highestprice       float(8,2) NOT NULL ,
